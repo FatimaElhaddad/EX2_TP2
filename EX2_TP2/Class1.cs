ec622bd46f33da4c2ae6c3328157ce318a8393c0
FatimaELhaddad <fatima.elhaddad3@usmba.ac.ma>
2022-11-11 00:33:52 +0100

    Ajoutez des fichiers projet.

---
+++ EX2_TP2/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2_TP2
+{
+    public class point
+    {
+        private double x, y;
+        public void setX(double p)
+        {
+            x = p;
+        }
+        public void setY(double p)
+        {
+            y = p;
+        }
+        public double getX()
+        {
+            return x;
+        }
+        public double getY()
+        {
+            return y;
+        }
+        public point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public bool equal(point p)
+        {
+            if (this.x == p.x && this.y == p.y)
+                return true;
+            else
+                return false;
+        }
+        public double distance(point p)
+        {
+            double dx = this.x - p.x;
+            double dy = this.y = p.y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        public void translation(double p)
+        {
+            x += p;
+            y += p;
+
+        }
+        public void rotation(double ang)
+        {
+            x = x * Math.Cos(ang) - y * Math.Sin(ang);
+            y = x * Math.Cos(ang) - y * Math.Sin(ang);
+        }
+
+        
+    }
+}
---
+++ EX2_TP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2_TP2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y,t ;
+            point p = new point(1.5,1.5);
+            Console.Out.Write("Donner l'abscisse: ");
+            x = double.Parse(Console.In.ReadLine());
+            Console.Out.Write("Donner l'ordonne: ");
+            y = double.Parse(Console.In.ReadLine());
+            p.setX(x);
+            p.setY(y);
+            point q = new point(0.5, 0.5);
+            Console.Out.Write("La distance(p,q) est "  +p.distance(q));
+            if (p.equal(q))
+                Console.Out.Write("p et q sont egaux ");
+            else
+                Console.Out.Write("p et q sont egaux ");
+            t = p.translation(10);
+            Console.Out.Write("Translation du point B par 10 "+t);
+            Console.Out.Write("Rotation du point B par 45deg " + rotation(45);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
